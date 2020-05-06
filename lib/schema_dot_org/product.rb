aaae5077ecbda9bff3992ed1cb39e310586c2e10
Tigran Apoyan <apoyan.tigran@gmail.com>
2020-05-06 11:50:26 +0400

    Add image field

--- lib/schema_dot_org/product.rb
+++ lib/schema_dot_org/product.rb
@@ -10,12 +10,14 @@ module SchemaDotOrg
                   :aggregate_rating,
                   :offers
 
+    validates :image, type: String, allow_nil: true
     validates :name, type: String
     validates :aggregate_rating, type: SchemaDotOrg::AggregateRating
     validates :offers, type: SchemaDotOrg::AggregateOffer
 
     def _to_json_struct
       {
+        "image" => image,
         "name" => name,
         "aggregateRating" => aggregate_rating.to_json_struct,
         "offers" => offers.to_json_struct
